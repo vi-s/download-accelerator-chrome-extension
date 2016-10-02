063468f288654c85a12c48ac7fba5c387ee8dfc4
Vikram Somu <vikster93@gmail.com>
2016-10-02 13:09:36 -0700

    axel dependency installer

--- host/install_host.sh
+++ host/install_host.sh
@@ -37,3 +37,29 @@ sed -i -e "s/HOST_PATH/$ESCAPED_HOST_PATH/" "$TARGET_DIR/$HOST_NAME.json"
 chmod o+r "$TARGET_DIR/$HOST_NAME.json"
 
 echo "Native messaging host $HOST_NAME has been installed."
+
+# Install Axel
+
+PKGMANAGER=undefined
+
+case "$(uname -s)" in
+   Darwin)
+    echo 'OS X DETECTED'
+    PKGMANAGER=brew
+    command -v axel || $PKGMANAGER update
+    ;;
+   Linux)
+    echo 'Linux DETECTED'
+    PKGMANAGER="sudo apt-get -yf"
+    command -v axel || sudo apt-get update
+    ;;
+   CYGWIN*|MINGW32*|MSYS*)
+    echo 'MS Windows DETECTED'
+    ;;
+   *)
+    echo 'other OS DETECTED' 
+    ;;
+esac
+
+# Dependency install
+command -v axel >/dev/null || { echo 'INSTALLING AXEL...'; $PKGMANAGER install axel; }
