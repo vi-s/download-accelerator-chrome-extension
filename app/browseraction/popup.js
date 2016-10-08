03d92a0ea96bcc18b24d4cd58612cac2f78ccb03
Vikram Somu <vikster93@gmail.com>
2016-10-08 03:04:44 -0700

    refactor ui dl manager class out

--- app/browseraction/popup.js
+++ app/browseraction/popup.js
@@ -2,48 +2,51 @@
 // Angular Setup
 angular.module('DownloadAccelerator', []);
 
-angular.module('DownloadAccelerator').controller('progressController', function($scope) {
-  $scope.test = '...test from ctrl';
-  $scope.downloadsMap = {}; // read from local storage on popup init, before any progress msgs from bg page
-  $scope.downloadsList = [];
-
-  class DownloadsStateUIManager {
-    constructor() {
-      this.downloadsMap = $scope.downloadsMap;
-      this.downloadsList = $scope.downloadsList;
-    }
-
-    onDownloadStateMsg(stateMsg) {
-      // stringifed timestamp -> date obj
-      if (stateMsg && stateMsg.fileInfo && stateMsg.fileInfo.dateTimeAdded) {
-        stateMsg.fileInfo.dateTimeAdded = new Date(JSON.parse(stateMsg.fileInfo.dateTimeAdded));
-      }
-
-      if (!this.downloadsMap[stateMsg.fileInfo.id]) {
-        stateMsg.fileInfo.arrayIdx = this.downloadsList.length;
-        this.downloadsList.push(stateMsg);
-      } else {
-        let oldState = this.downloadsMap[stateMsg.fileInfo.id];
-        let arrayIdx = oldState.fileInfo.arrayIdx;
-        stateMsg.fileInfo.arrayIdx = arrayIdx;
-        this.downloadsList[arrayIdx] = stateMsg;
-      }
-      this.downloadsMap[stateMsg.fileInfo.id] = stateMsg;
-      $scope.$apply();
-    }
-
-
+class DownloadsStateUIManager {
+  constructor(downmap, downlist) {
+    this.downloadsMap = downmap;
+    this.downloadsList = downlist;
   }
 
-  let dsui = new DownloadsStateUIManager();
+  onInit() {
+    console.log('I HAVE BEEN INIT');
+  }
 
-  chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+  onDownloadStateMsg(stateMsg, cb) {
+    // stringifed timestamp -> date obj
+    if (stateMsg && stateMsg.fileInfo && stateMsg.fileInfo.dateTimeAdded) {
+      stateMsg.fileInfo.dateTimeAdded = new Date(JSON.parse(stateMsg.fileInfo.dateTimeAdded));
+    }
+
+    if (!this.downloadsMap[stateMsg.fileInfo.id]) {
+      stateMsg.fileInfo.arrayIdx = this.downloadsList.length;
+    } else {
+      let oldState = this.downloadsMap[stateMsg.fileInfo.id];
+      stateMsg.fileInfo.arrayIdx = oldState.fileInfo.arrayIdx;
+    }
+
+    this.downloadsList[stateMsg.fileInfo.arrayIdx] = stateMsg;
+    this.downloadsMap[stateMsg.fileInfo.id] = stateMsg;
+    cb();
+  }
+
+}
+
+angular.module('DownloadAccelerator').controller('progressController', function($scope) {
+  $scope.test = '...test from ctrl';
+  // read from local storage on popup init, before any progress msgs from bg page
+  $scope.downloadsMap = {};
+  $scope.downloadsList = [];
+
+  let dsui = new DownloadsStateUIManager($scope.downloadsMap, $scope.downloadsList);
+  dsui.onInit();
+
+  chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
     switch(request.type) {
       case "dlprogmsg":
-        // document.getElementById('progContainer').innerHTML = JSON.stringify(request.state);
-        console.log('msg recvd');
-        console.log('dl list len:', $scope.downloadsList.length);
-        dsui.onDownloadStateMsg(request.state);
+        dsui.onDownloadStateMsg(request.state, () => {
+          $scope.$apply();
+        });
         break;
       default:
         break;
