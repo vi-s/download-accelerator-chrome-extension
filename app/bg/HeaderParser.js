9a997716bf56c157251c7994fb8fe8681cf1b118
Vikram Somu <vikster93@gmail.com>
2017-06-25 16:00:13 -0400

    updated download file link catching methodology and logic

---
+++ app/bg/HeaderParser.js
@@ -0,0 +1,63 @@
+
+export default class HeaderParser {
+
+  constructor() {
+    this.defaultFileName = 'download';
+  }
+
+  extractHeaders(details) {
+    let headerMap = {};
+    details.responseHeaders.forEach((e) => {
+      headerMap[e.name.toLowerCase()] = e.value; // case insensitivize http headers
+    });
+    return headerMap;
+  }
+
+  testContentTypeDownloadable(contentType) {
+    // check for audio and video mime types
+    if (/^(audio|video)/.test(contentType)) {
+      return true;
+    }
+    // check application subcases for downloadable file types
+    if (/^application\/.*(octet\-?stream|compressed|rar|tar|zip|7z).*/.test(contentType)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  getFileNameFromHeaders(resHeaderMap, details) {
+    let fileName;
+    // first check content disposition header for attachment filename
+    if (resHeaderMap['content-disposition']) {
+      fileName = resHeaderMap['content-disposition']
+        .replace('attachment; filename=', '')
+        .replace(/['"]/g, '');
+    }
+    // check for redirection header
+    else if(resHeaderMap['location']) {
+      fileName = this.getFileNameFromPath(resHeaderMap['location']);
+    }
+    // use url as path for filename extraction
+    else {
+      fileName = this.getFileNameFromPath(details.url);
+    }
+
+    return this.validateFileName(fileName) ? fileName : this.defaultFileName;
+  }
+
+  // downloadable files usually have a file extension at least 2 chars long following a name
+  validateFileName(fileName) {
+    return /^.+\.[a-zA-Z0-9]{2,}$/.test(fileName);
+  }
+
+  // Also works on URLs
+  getFileNameFromPath(path) {
+    // if last char is / or \, exclude the slash
+    if (/[\/\\]/.test(path[path.length - 1])) path = path.slice(0,path.length - 1);
+    // remove all contents before final slash.
+    return path.replace(/^.*[\\\/]/, '')
+  }
+
+}
+
--- app/bg/background.js
+++ app/bg/background.js
@@ -1,6 +1,9 @@
 import NativeMessageBroker from './NativeMessageBroker';
+import HeaderParser from './HeaderParser';
 
 const nativeMsgBroker = new NativeMessageBroker();
+const headerParser = new HeaderParser();
+
 nativeMsgBroker.connect();
 
 // message from popup.js
@@ -25,60 +28,92 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   return true;
 });
 
-chrome.webRequest.onBeforeRequest.addListener(function(details){
-  //console.log(JSON.stringify(details));
-  // keep2share support
-  if (details.url.indexOf('keep') > -1 && details.url.indexOf('name=') > -1) {
-    let split = details.url.split('name=');
-    if (!(split.length > 1)) {
-      return {};
-    }
-    var fileName = split[1];
+chrome.webRequest.onHeadersReceived.addListener((details) => {
+  let resHeaderMap = headerParser.extractHeaders(details);
+  // handle requests with missing content-type header... catch or not catch missing content-type?
+  // if no content-type header, give it application/octet-stream and catch the request for download
+  // if (!resHeaderMap['content-type']) resHeaderMap['content-type'] = 'application/octet-stream';
+  
+  if (headerParser.testContentTypeDownloadable(resHeaderMap['content-type'])) {
+    let fileName = headerParser.getFileNameFromHeaders(resHeaderMap, details);
+    let fileSize = resHeaderMap['content-length']; // find use for this later
     nativeMsgBroker.sendDownloadInitNativeMsg(fileName, details);
-  } else if (details.url.indexOf('joker') > -1 && /\/\/fs[0-9 ]{2}\.filejoker/.test(details.url)) {
-    let split = details.url.split('/');
-    var fileName = split[split.length - 1]
-
-    chrome.cookies.get({
-      url: 'https://filejoker.net',
-      name: 'joker_cook'
-    }, (cookie) => {
-      let cookieHeaderStr = cookie ? `Cookie: ${cookie.name}=${cookie.value}` : '';
-      nativeMsgBroker.sendDownloadInitNativeMsg(fileName, details, cookieHeaderStr);
-    });
-
-  } else if (details.url.indexOf('138.68.41.100') > -1) {
-    let split = details.url.split('/');
-    var fileName = split[split.length - 1];
-
-    if (! /.+\.[a-zA-Z]{2,}/.test(fileName)) {
-      return {};
-    }
-
-    nativeMsgBroker.sendDownloadInitNativeMsg(fileName, details);
-  } else {
-    return {};
+    /**
+     * BLOCKING RESPONSE
+     * silently cancel the request
+     * `cancel: true` param redirects to 'blocked by extension' page, not silent cancellation
+     */
+    return {
+      redirectUrl: 'javascript:'
+    };
   }
 
-  // silently cancel the request
-  let blockingResponse = {
-    redirectUrl: 'javascript:'
-  };
-
-  return blockingResponse;
+  return {};
 },
 // Request Filter
 {
   urls: [
-    // '<all_urls>',
-    '*://*.keep2share.cc/*',
-    '*://*.filejoker.net/*',
-    '*://138.68.41.100/*'
+    '<all_urls>'
   ],
   types: [ 'main_frame' ]
 },
 // opt_extraInfoSpec array
-['requestBody','blocking']);
+["blocking", "responseHeaders"]);
+
+// chrome.webRequest.onBeforeRequest.addListener(function(details){
+//   //console.log(JSON.stringify(details));
+//   // keep2share support
+//   if (details.url.indexOf('keep') > -1 && details.url.indexOf('name=') > -1) {
+//     let split = details.url.split('name=');
+//     if (!(split.length > 1)) {
+//       return {};
+//     }
+//     var fileName = split[1];
+//     nativeMsgBroker.sendDownloadInitNativeMsg(fileName, details);
+//   } else if (details.url.indexOf('joker') > -1 && /\/\/fs[0-9 ]{2}\.filejoker/.test(details.url)) {
+//     let split = details.url.split('/');
+//     var fileName = split[split.length - 1]
+
+//     chrome.cookies.get({
+//       url: 'https://filejoker.net',
+//       name: 'joker_cook'
+//     }, (cookie) => {
+//       let cookieHeaderStr = cookie ? `Cookie: ${cookie.name}=${cookie.value}` : '';
+//       nativeMsgBroker.sendDownloadInitNativeMsg(fileName, details, cookieHeaderStr);
+//     });
+
+//   } else if (details.url.indexOf('138.68.41.100') > -1) {
+//     let split = details.url.split('/');
+//     var fileName = split[split.length - 1];
+
+//     if (! /.+\.[a-zA-Z]{2,}/.test(fileName)) {
+//       return {};
+//     }
+
+//     nativeMsgBroker.sendDownloadInitNativeMsg(fileName, details);
+//   } else {
+//     return {};
+//   }
+
+//   // silently cancel the request
+//   let blockingResponse = {
+//     redirectUrl: 'javascript:'
+//   };
+
+//   return blockingResponse;
+// },
+// // Request Filter
+// {
+//   urls: [
+//     // '<all_urls>',
+//     '*://*.keep2share.cc/*',
+//     '*://*.filejoker.net/*',
+//     '*://138.68.41.100/*'
+//   ],
+//   types: [ 'main_frame' ]
+// },
+// // opt_extraInfoSpec array
+// ['requestBody','blocking']);
 
 
 /* WEB REQUEST HEADER INTERCEPTION/MODIFICATION EXAMPLE */
